8a5e88c95312654fdcaf96b33753b233296b0c45
MirzaabdullayevTest <mirzaabdullayevtest@mail.ru>
2022-02-07 00:41:55 +0500

    port has been changed

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 const app = require('./app')
-const PORT = process.env.PORT || 3000;
+const PORT = normalizePort(process.env.PORT || '3000');
+
+app.set('port', PORT);
+
+function normalizePort(val) {
+    const PORT = parseInt(val, 10);
+
+    if (isNaN(PORT)) {
+        // named pipe
+        return val;
+    }
+
+    if (PORT >= 0) {
+        // port number
+        return PORT;
+    }
+
+    return false;
+}
 
 app.listen(PORT, () => { console.log('Server has been started on port', PORT) })
